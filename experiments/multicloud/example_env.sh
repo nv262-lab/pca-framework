d3580a2e6e1bd9d917a196ea9c46ee499d15dbe6
nv262-lab <nv262@cornell.edu>
2025-12-30 13:41:35 -0800

    Add example_env.sh for cloud provider variables
    Provides example environment variable configurations for AWS, GCP, and Azure.

---
+++ experiments/multicloud/example_env.sh
@@ -0,0 +1,13 @@
+# Example environment variables for cloud providers
+# AWS
+# export AWS_ACCESS_KEY_ID=...
+# export AWS_SECRET_ACCESS_KEY=...
+# export AWS_SESSION_TOKEN=...  # optional
+
+# GCP
+# export GOOGLE_APPLICATION_CREDENTIALS="/path/to/service-account.json"
+
+# Azure
+# export AZURE_STORAGE_CONNECTION_STRING="DefaultEndpointsProtocol=...;AccountName=...;AccountKey=..."
+# or
+# export AZURE_STORAGE_KEY="..."
